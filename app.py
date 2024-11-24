02469009db4202dd745d6ec9bad165c734f864b9
walton <zjxcdut@gmail.com>
2024-11-24 01:35:27 -0400

    Finsh the UI for  index page

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, render_template
+# from transformers import pipeline
 
 app = Flask(__name__)
+
+# tts_pipeline = pipeline("text-to-speech", model="coqui/XTTS-v2")
 
 @app.route('/')
 def home():
